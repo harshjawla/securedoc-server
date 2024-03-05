2cc5cd65c6c51016aa8b62319fea7d975ce368eb
Harsh Jawla <harshjawla95@gmail.com>
2024-03-05 21:29:37 +0530

    updated cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
