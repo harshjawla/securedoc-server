2e174fb631b336de71b6e759bce8a3c6fd886675
Harsh Jawla <harshjawla95@gmail.com>
2024-03-05 22:01:34 +0530

    updated cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://securedoc-client.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Origin",
